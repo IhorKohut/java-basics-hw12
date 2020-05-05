dbf1879ef0bfca5ab6d175a5ce0d55a16e4cf323
IhorKohut <ih.kohut@gmail.com>
2020-05-05 16:47:57 +0300

    change the HW12

--- src/StreamTask.java
+++ src/StreamTask.java
@@ -1,10 +1,4 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 
 public class StreamTask {
 
@@ -17,12 +11,10 @@ public class StreamTask {
     }
 
     public static int[] getSortedOddsArray(int[] numbers) {
-        IntStream.of(numbers)
-                .filter(x -> x % 2 == 1 || x % 2 == -1)
-                //.forEach(System.out::println);
-                .collect(Collectors.toList());
 
-        // TODO replace return with your code
-        return new int[]{};
+        return IntStream.of(numbers)
+                .filter(x -> x % 2 == 1 || x % 2 == -1)
+                .sorted()
+                .toArray();
     }
 }
